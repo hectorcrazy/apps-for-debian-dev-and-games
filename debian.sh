7449ef25470b9e7ee863c6a5058d89fbae4bd501
Hectorcrazy <94460102+hectorcrazy@users.noreply.github.com>
2022-05-07 18:34:13 -0500

    Create debian.sh

---
+++ debian.sh
@@ -0,0 +1,9 @@
+sudo apt-get upgrade
+sudo apt-get update
+sudo apt install codeblocks
+sudo apt install bluefish
+sudo apt install geany
+sudo apt install mame
+sudo apt install zsnes
+sudo apt install fceux
+echo "Instalados\nCreado por El HectorCrazy";
